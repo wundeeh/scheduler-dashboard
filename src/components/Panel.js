cf18fa90561bda73ccf1b22ccd46d4840c31ef08
wundeeh <jakiriksh@gmail.com>
2023-01-19 15:25:59 -0600

    create a panel frame

---
+++ src/components/Panel.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Panel extends Component {
+  render() {
+    const { id, label, value, onSelect } = this.props;
+
+    return (
+      <section className="dashboard__panel" onClick={(event) => onSelect(id)}>
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
+
+export default Panel;
